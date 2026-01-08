ac6e10396fe0a2989859df735e56d3f561398482
Liel Yehoshua <95339267+liel-yehoshua-sp@users.noreply.github.com>
2026-01-08 15:40:05 +0200

    Task 2 completed

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+app_mode = os.getenv("APP_MODE", "Production")
+api_key = os.getenv("API_KEY", "MISSING")
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "ok",
+        "app_mode": app_mode,
+        "api_key_present": api_key != "MISSING"
+    })
+
+@app.route("/crash")
+def crash():
+    raise RuntimeError("Intentional crash")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
